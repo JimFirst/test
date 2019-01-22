5e19784ddba41cd0593d9973fcdcfeb2206a27e5
JimDong <154211320@qq.com>
2019-01-22 19:08:46 +0800

    first commit

---
+++ src/components/common/child.vue
@@ -0,0 +1,50 @@
+<template>
+    <section>
+        <input
+        v-bind:value="value"
+        v-on:input="$emit('input', $event.target.value)"
+        >
+        <button>{{demo}}</button>
+        <p>test:{{this.childTest}}</p>
+        <input type="text" v-model="ipt.value1">
+        <input type="text" v-model="ipt.value2">
+    </section>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            demo:this.value,
+            ipt:{
+                value1:''
+            },
+            childTest:this.test
+        }
+    },
+    props:{
+        value:{
+            type:String
+        },
+        test:{
+            type:Boolean,
+            default:false  
+        }
+    },
+    methods:{
+        set(val){
+            this.$data.ipt.value1=val
+        }
+    },
+    watch:{
+        test:function(val){
+            console.log(val)
+            this.$data.childTest=val;
+        }
+    }
+}
+</script>
+<style>
+
+</style>
+
+
---
+++ src/components/common/steps.vue
@@ -0,0 +1,58 @@
+<template>
+    <section>
+        <div class="norm-steps" >
+            <div class="step" v-for="item in childSteps" :key="item.value">
+                <div class="top">{{item.name}}</div>
+                <div class="line"></div>
+                <div class="bottom">{{item.name}}</div>
+                <div class="bottom">{{item.name}}</div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      childSteps:this.steps
+    }
+  },
+  props:['steps'],
+  watch:{
+        steps:function(val){
+            console.log(val)
+            this.$data.childSteps=val;
+        }
+    }
+}
+</script>
+
+<style>
+    
+    .norm-steps{
+        position: relative;
+        display: flex;
+        width: 100%;
+        height: 60px;
+        border: 1px solid red;
+        flex-direction: row;        
+    }
+    .norm-steps .step{
+        border: 1px solid yellow;
+        height: 100%;
+        flex: 1;
+    }
+    .norm-steps .line{
+        position: absolute;
+        width: 100%;
+        top: 50%;
+        left: 0;;
+        border-top: 1px solid black;
+    }
+    .norm-steps .top,.bottom{
+        line-height: 20px;
+        height: 20px;
+        font-size: 12px;
+    }
+</style>
